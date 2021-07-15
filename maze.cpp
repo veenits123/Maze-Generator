ed4f662c70b4e7a42b341f1d80d8e6af1beb1889
veenits123 <123sveenit@gmail.com>
2021-07-15 14:49:00 +0530

    cleaning up

--- maze.cpp
+++ maze.cpp
@@ -4,24 +4,24 @@ class DSU {
 private:
     vector<int> parent, rank;
     int findSet(int u) {
-        return u == parent[u] ? u: (parent[u] = findSet(parent[u]));
+        return u == parent[u] ? u : (parent[u] = findSet(parent[u]));
     }
 
 public:
     DSU(int n) {
         parent.resize(n);
         rank.resize(n);
-        for(int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
             parent[i] = i;
     }
-    
+
     bool sameSet(int u, int v) {
         return findSet(u) == findSet(v);
     }
 
     void unite(int u, int v) {
         int pu = findSet(u), pv = findSet(v);
-        if(rank[pu] > rank[pv]) {
+        if (rank[pu] > rank[pv]) {
             parent[pv] = pu;
         }
         else {
@@ -39,23 +39,22 @@ struct Wall {
 };
 
 Maze::Maze(int _height, int _width) {
-    
     srand(time(0));
     height = _height;
     width = _width;
     maze.assign(height, vector<int>(width));
 
-    for(int i = 0; i < 4; i++)
+    for (int i = 0; i < 4; i++)
         directions[i] = 1 << i;
-    
+
     dx[U] = -1, dx[R] = 0, dx[D] = 1, dx[L] = 0;
     dy[U] = 0, dy[R] = 1, dy[D] = 0, dy[L] = -1;
     opposite[U] = D, opposite[R] = L,
-    opposite[D] = U, opposite[L] = R;
+                                   opposite[D] = U, opposite[L] = R;
 }
 
 void Maze::reset() {
-    maze.assign(height, vector<int>(width,0));
+    maze.assign(height, vector<int>(width, 0));
 }
 
 bool Maze::out_of_bounds(int nx, int ny) {
@@ -65,14 +64,14 @@ bool Maze::out_of_bounds(int nx, int ny) {
 void Maze::draw_ascii() {
     // cout << string(width * 2 + 1, '_') << endl;
     cout << " ";
-    for(int j = 0; j < width; j++)
+    for (int j = 0; j < width; j++)
         cout << "_ ";
     cout << endl;
-    for(int i = 0; i < height; i ++) {
+    for (int i = 0; i < height; i ++) {
         cout << "|";
-        for(int j = 0; j < width; j ++) {
-            cout << ((maze[i][j] & D) ? ' ': '_');
-            cout << ((maze[i][j] & R) ? ' ': '|');
+        for (int j = 0; j < width; j ++) {
+            cout << ((maze[i][j] & D) ? ' ' : '_');
+            cout << ((maze[i][j] & R) ? ' ' : '|');
         }
         cout << endl;
     }
@@ -80,11 +79,11 @@ void Maze::draw_ascii() {
 
 void Maze::recursive_backtrack_dfs(int x, int y) {
     random_shuffle(directions, directions + 4);
-    for(int dir : directions) {
+    for (int dir : directions) {
         int nx = dx[dir] + x;
         int ny = dy[dir] + y;
 
-        if(out_of_bounds(nx, ny) || maze[nx][ny]) {
+        if (out_of_bounds(nx, ny) || maze[nx][ny]) {
             continue;
         }
 
@@ -102,24 +101,23 @@ int Maze::cellIndex(int x, int y) {
     return x * width + y;
 }
 
-
 void Maze::kruskal() {
     DSU dsu(height * width);
     vector<Wall> walls;
-    for(int i = 0; i < height; i++) {
-        for(int j = 0; j < width; j++) {
-            if(j) walls.push_back(Wall(i, j, L));
-            if(i) walls.push_back(Wall(i, j, U));
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (j) walls.push_back(Wall(i, j, L));
+            if (i) walls.push_back(Wall(i, j, U));
         }
     }
 
     random_shuffle(walls.begin(), walls.end());
 
-    for(auto wall: walls) {
+    for (auto wall : walls) {
         int x = wall.x, y = wall.y, dir = wall.dir;
         int nx = dx[dir] + x, ny = dy[dir] + y;
         int cur = cellIndex(x, y), nxt = cellIndex(nx, ny);
-        if(!dsu.sameSet(cur, nxt)) {
+        if (!dsu.sameSet(cur, nxt)) {
             dsu.unite(cur, nxt);
             maze[x][y] |= dir;
             maze[nx][ny] |= opposite[dir];
--- maze.h
+++ maze.h
@@ -1,13 +1,14 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
 
 class Maze {
 
 private:
 
-    static const int _S=20, U=1, R=2, D=4, L=8;
+    static const int _S = 20, U = 1, R = 2, D = 4, L = 8;
     int dx[_S], dy[_S], opposite[_S], directions[4];
-    
+
     int width, height;
     string algo;
     vector<vector<int>> maze;
@@ -17,7 +18,7 @@ private:
     void recursive_backtrack_dfs(int x, int y);
 
 public:
-    
+
     Maze(int _height, int _width);
     void reset();
     void draw_ascii();
